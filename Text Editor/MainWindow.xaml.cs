cad3244f86efc1915da9e269dd09d3216c3ac777
Uximy <48489781+Uximy@users.noreply.github.com>
2020-11-20 15:46:01 +0600

    Add files via upload

--- Text Editor/MainWindow.xaml.cs
+++ Text Editor/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.Win32;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Threading;
+using System.Globalization;
 
 namespace Text_Editor
 {
@@ -26,12 +30,17 @@ namespace Text_Editor
       InitializeComponent();
       cmbFontFamily.ItemsSource = Fonts.SystemFontFamilies.OrderBy(f => f.Source);
       cmbFontSize.ItemsSource = new List<double>() { 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72 };
+      //Thread.CurrentThread.CurrentCulture = new CultureInfo("ru");
+      //MenuOpen.Header = Strings.MenuOpen;
+      //MenuSave.Header = Strings.MenuSave;
     }
     private void rtbEditor_SelectionChanged(object sender, RoutedEventArgs e)
     {
       object temp = rtbEditor.Selection.GetPropertyValue(Inline.FontWeightProperty);
-      temp = rtbEditor.Selection.GetPropertyValue(Inline.TextDecorationsProperty);
 
+      btnBold.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(FontWeights.Bold));
+      temp = rtbEditor.Selection.GetPropertyValue(Inline.FontStyleProperty);
+      btnItalic.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(FontStyles.Italic));
       temp = rtbEditor.Selection.GetPropertyValue(Inline.FontFamilyProperty);
       cmbFontFamily.SelectedItem = temp;
       temp = rtbEditor.Selection.GetPropertyValue(Inline.FontSizeProperty);
@@ -43,11 +52,106 @@ namespace Text_Editor
     {
       if (cmbFontFamily.SelectedItem != null)
         rtbEditor.Selection.ApplyPropertyValue(Inline.FontFamilyProperty, cmbFontFamily.SelectedItem);
+      rtbEditor.Focus();
     }
 
     private void cmbFontSize_TextChanged(object sender, TextChangedEventArgs e)
     {
       rtbEditor.Selection.ApplyPropertyValue(Inline.FontSizeProperty, cmbFontSize.Text);
+      rtbEditor.Focus();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+      OpenFileDialog openFile = new OpenFileDialog();
+      openFile.Filter = "Rich Text Format| *.rtf";
+      if (openFile.ShowDialog() == true)
+      {
+        FileStream filestream = new FileStream(openFile.FileName, FileMode.Open);
+        TextRange textrg = new TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd);
+        textrg.Load(filestream, DataFormats.Rtf);
+      }
+    }
+
+    private void Button_Click_1(object sender, RoutedEventArgs e)
+    {
+      SaveFileDialog saveFile = new SaveFileDialog();
+      saveFile.Filter = "Rich Text Format| *.rtf";
+      if (saveFile.ShowDialog() == true)
+      {
+        FileStream filestream = new FileStream(saveFile.FileName, FileMode.Create);
+        TextRange textrgsave = new TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd);
+        textrgsave.Save(filestream, DataFormats.Rtf);
+      }
+    }
+
+    private void RusButton_Click(object sender, RoutedEventArgs e)
+    {
+      //Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+      CultureInfo newCulture = CultureInfo.CreateSpecificCulture("ru");
+      Thread.CurrentThread.CurrentUICulture = newCulture;
+      MenuCreate.Header = Strings.MenuCreate;
+      MenuOpen.Header = Strings.MenuOpen;
+      MenuSave.Header = Strings.MenuSave;
+      MenuSaveAs.Header = Strings.MenuSaveAs;
+      MenuOptions.Header = Strings.MenuOptions;
+      MenuPrinting.Header = Strings.MenuPrinting;
+      MenuExit.Header = Strings.MenuExit;
+
+
+      MenuCancel.Header = Strings.MenuCancel;
+      MenuTocut.Header = Strings.MenuTocut;
+      MenuCopy.Header = Strings.MenuCopy;
+      MenuPaste.Header = Strings.MenuPaste;
+      MenuDelete.Header = Strings.MenuDelete;
+      MenuSelectAll.Header = Strings.MenuSelectAll;
+
+
+      MenuFonts.Header = Strings.MenuFonts;
+      MenuWordWrap.Header = Strings.MenuWordWrap;
+
+
+      MenuFile.Header = Strings.MenuFile;
+      MenuEdit.Header = Strings.MenuEdit;
+      MenuFormat.Header = Strings.MenuFormat;
+
+
+      LbColumn.Content = Strings.LbColumn;
+      LbRow.Content = Strings.LbRow;
+    }
+
+    private void EngButton_Click(object sender, RoutedEventArgs e)
+    {
+      //Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+      CultureInfo newCulture = CultureInfo.CreateSpecificCulture("en-US");
+      Thread.CurrentThread.CurrentUICulture = newCulture;
+      MenuCreate.Header = Strings.MenuCreate;
+      MenuOpen.Header = Strings.MenuOpen;
+      MenuSave.Header = Strings.MenuSave;
+      MenuSaveAs.Header = Strings.MenuSaveAs;
+      MenuOptions.Header = Strings.MenuOptions;
+      MenuPrinting.Header = Strings.MenuPrinting;
+      MenuExit.Header = Strings.MenuExit;
+
+
+      MenuCancel.Header = Strings.MenuCancel;
+      MenuTocut.Header = Strings.MenuTocut;
+      MenuCopy.Header = Strings.MenuCopy;
+      MenuPaste.Header = Strings.MenuPaste;
+      MenuDelete.Header = Strings.MenuDelete;
+      MenuSelectAll.Header = Strings.MenuSelectAll;
+
+
+      MenuFonts.Header = Strings.MenuFonts;
+      MenuWordWrap.Header = Strings.MenuWordWrap;
+
+      MenuFile.Header = Strings.MenuFile;
+      MenuEdit.Header = Strings.MenuEdit;
+      MenuFormat.Header = Strings.MenuFormat;
+
+
+      LbColumn.Content = Strings.LbColumn;
+      LbRow.Content = Strings.LbRow;
     }
   }
 }
